8030fef19eedb737af059c0cd46203c98420fce0
Ali Cagatay Uçar <alicagatayucar@gmail.com>
2015-05-11 00:33:47 +0300

    PointerBasedListe
    PointerBasedListe

---
+++ src/pointerbasedliste/Dugum.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pointerbasedliste;
+
+/**
+ *
+ * @author Egemen
+ */
+public class Dugum {
+    Dugum onceki,sonraki;
+    int value;
+    
+    public Dugum(int v) {
+        this.value = v;
+    }
+    
+}
